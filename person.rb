058b1a87b22e7b392da0283b18835aea5c03b33c
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-23 11:49:42 +0300

    Create classes using inheritance and encapsulation

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
+
+  def initialize(age, parent_permission = true, name = 'Unknown')
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def is_of_age?
+    if @age >= 18
+  end
+  private :is_of_age?
+
+  def can_use_services
+    if is_of_age? || @parent_permission
+  end
+  public :can_use_services?
+end
