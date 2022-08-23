34d3c38edad0190a8b93df5c63aff46a6e072c5c
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-23 12:39:44 +0300

    Fix linters

--- person.rb
+++ person.rb
@@ -2,14 +2,14 @@ class Person
   attr_reader :id
   attr_accessor :name, :age, :parent_permission
 
-  def initialize(age, parent_permission = true, name = 'Unknown')
+  def initialize(age, parent_permission: true, name: 'Unknown')
     @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
   private :is_of_age?
