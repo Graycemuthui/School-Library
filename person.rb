9b46d236586d675c92573eceaebcac01efe7b780
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-23 12:53:37 +0300

    Interchange the public nad private methods

--- person.rb
+++ person.rb
@@ -9,13 +9,13 @@ class Person
     @parent_permission = parent_permission
   end
 
-  def of_age?
-    @age >= 18
-  end
-  private :is_of_age?
-
   def can_use_services
     is_of_age? || @parent_permission
   end
-  public :can_use_services?
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
