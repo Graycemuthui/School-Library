ee352090221ad35e6c153c372a1648e7245cdde5
Robertson Arthur <robertsonakpan@gmail.com>
2022-08-29 14:53:52 +0100

    Add Options Class

--- options.rb
+++ options.rb
@@ -1,8 +1,11 @@
-module 
-def options(*methods)
-methods.each_with_index do |method, index|
- increment = index + 1
- 
- end
-end
+class Options
+  def callMethod(option, methods)
+    x = option()
+    puts x
+    methods.each_with_index do |method, index|
+      if x == index + 1
+        method
+      end
+    end
+  end  
 end
