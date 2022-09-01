1974842d3cd2bb949182b5604903807bd8fc878c
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-01 15:44:11 +0100

    Update Classroom test suite

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,22 +1,10 @@
 require './spec/spec_helper'
 
-# write tests for classroom class
 describe Classroom do
-  before do
-    @classroom = Classroom.new 'student'
-  end
-
-  describe '#new' do
-    it 'should return a new classroom object' do
-      @classroom.should be_an_instance_of Classroom
-    end
-
-    # it should return the correct student
-
-    describe '#student' do
-      it 'should return the correct student' do
-        @classroom.student.should eq ['student']
-      end
+  context 'given parameter' do
+    it 'should have name' do
+      class_room = Classroom.new('class B')
+      expect(class_room.label).to eql 'class B'
     end
   end
 end
