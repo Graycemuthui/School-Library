dc7615baf27e7d68c505340e90f841daa91302cf
Robertson Arthur <robertsonakpan@gmail.com>
2022-09-01 12:35:37 +0100

    Update Student class to receive optional id

--- student.rb
+++ student.rb
@@ -2,11 +2,12 @@ require_relative 'person'
 require_relative 'classroom'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :classroom, :type
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true, id = rand(1..1000))
+    super(age, name, parent_permission, id)
     @classroom = classroom
+    @type = 'student'
   end
 
   def play_hooky
