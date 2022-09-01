0a4e1bf5d7bcb766bb553806025172848235edd4
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-01 17:13:54 +0300

    create tests for classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,1 +1,22 @@
 require './spec/spec_helper'
+
+# write tests for classroom class
+describe Classroom do
+  before do
+    @classroom = Classroom.new 'student'
+  end
+
+  describe '#new' do
+    it 'should return a new classroom object' do
+      @classroom.should be_an_instance_of Classroom
+    end
+
+    # it should return the correct student
+
+    describe '#student' do
+      it 'should return the correct student' do
+        @classroom.student.should eq []
+      end
+    end
+  end
+end
